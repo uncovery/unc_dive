22f26793a436b85d7bf900021b83519cdbca137d
Uncovery <minecraft@uncovery.me>
2018-08-17 16:48:57 +0800

    update class

--- examples/example.php
+++ examples/example.php
@@ -1,17 +1,40 @@
 <?php
 
-require_once('./../unc_dive.inc.php');
-
-$source_format = 'Suunto_D4i';
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 
 // please provide your own backup file from the D4i computer
-$source_file = './sample.db';
+$data = "./default.db";
+$type = "Suunto_D4i";
 
-// list all dives 
-$avail_dives = unc_dive_list($source_format, $source_file);
+// requiring class...
+require_once('./../unc_dive.inc.php');
 
-// get the id of the latest dive
-$latest_dive_id = unc_dive_latest($source_format, $source_file);
+// creating instance....
+$dive = new unc_dive();
+var_dump($dive);
 
-// get the dive data for the latest dive
-$data = unc_dive_get_one($source_format, $source_file, $latest_dive_id);
+$dive->set_debug(false);
+
+$dive->set_computer_type($type);
+$dive->set_source($data);
+
+$latest = $dive->get_latest_dive_id();
+
+echo "latest dive ID: $latest\n";
+// var_dump($latest);
+
+$list_dives = $dive->dive_list();
+
+$no_dives = count($list_dives);
+
+echo "$no_dives dives found!\n";
+// var_dump($list_dives);
+
+$dive_data = $dive->dive_get_one($latest);
+
+// var_dump($dive_data);
+
+$no_dive_points = count($dive_data);
+
+echo "$no_dive_points dive data found\n";
